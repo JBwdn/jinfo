2d561c83eeeccc259c34953e804b59817678a486
Jake Bowden <jake.bowden95@gmail.com>
2020-11-01 12:10:10 +0000

    added utils module

--- jinformatics/__init__.py
+++ jinformatics/__init__.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python
 
 from .sequence import DNASequence, RNASequence, AASequence
+from .utils import one_hot_dna, random_dna
---
+++ jinformatics/utils.py
@@ -0,0 +1,30 @@
+def one_hot_dna(input_seq_str: str, max_seq_len: int):
+    """
+    One hot encode a string format dna sequence.
+    Add zero padding up to the maximum length.
+
+    """
+    import numpy as np
+
+    encode_dict = {
+        "A": [1, 0, 0, 0],
+        "T": [0, 1, 0, 0],
+        "C": [0, 0, 1, 0],
+        "G": [0, 0, 0, 1],
+        "X": [0, 0, 0, 0]
+    }
+    input_seq_upper = input_seq_str.upper()
+    padding = "".join(["X" for i in range(max_seq_len - len(input_seq_str))])
+    encoded_dna = [encode_dict[base] for base in input_seq_upper + padding]
+    np_encoded = np.array(encoded_dna, dtype=int)
+    return np_encoded.flatten()
+
+
+def random_dna(seq_length: int) -> str:
+    dna_base_list = ["A", "T", "C", "G"]
+    seq_list = [random.choice(dna_base_list) for i in range(seq_length)]
+    return "".join(seq_list)
+
+
+if __name__ == "__main__":
+    pass
