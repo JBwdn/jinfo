8b69d1efe2104b3899676936cd3fba0c08540fb9
JBwdn <jake.bowden95@gmail.com>
2020-11-03 14:38:18 +0000

    removed old files

--- testing_script.py
+++
@@ -1,6 +0,0 @@
-import jinfo as j
-
-clf_aa_list = j.seq_list_from_fasta("CLF_known.fasta")
-clf_alignment = j.multialign(clf_aa_list, maxiters=1)
-clf_tree = clf_alignment.calc_tree()
-print(clf_tree.tree)
