096a33b46b75658afc5dd2f4432f278656d9ba80
Jake Bowden <jake.bowden95@gmail.com>
2020-11-22 00:43:57 +0000

    modified __init__.py files

--- jinfo/__init__.py
+++ jinfo/__init__.py
@@ -3,16 +3,19 @@ from .alignment import BaseAlignment
 from .phylogenetics import PhyloTree
 from .metabolite import BaseMetabolite
 
-from .utils.one_hot_dna import one_hot_dna
-from .utils.random_DNASeq import random_DNASeq
-from .utils.DNASeq_from_NCBI import DNASeq_from_NCBI
-from .utils.seq_list_to_fasta import seq_list_to_fasta
-from .utils.seq_list_from_fasta import seq_list_from_fasta
-from .utils.seq_from_fasta import seq_from_fasta
-from .utils.alignment_from_fasta import alignment_from_fasta
-from .utils.multialign import multialign
-from .utils.calc_phylo_tree import calc_phylo_tree
-from .utils.percentage_identity import percentage_identity
-from .utils.remove_degenerate_seqs import remove_degenerate_seqs
+from .utils import (
+    one_hot_dna,
+    random_DNASeq,
+    DNASeq_from_NCBI,
+    seq_list_to_fasta,
+    seq_list_from_fasta,
+    remove_degenerate_seqs,
+    percentage_identity,
+    seq_from_fasta,
+    alignment_from_fasta,
+    multialign,
+    calc_phylo_tree,
+)
+
 
 from .tables import DNA_VOCAB, RNA_VOCAB, AA_VOCAB, CODON_TABLE
